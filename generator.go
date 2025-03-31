bb1e46ecb7243d3b9d52642e20d8cc9a7474cb41
storezhang <storezhang@gmail.com>
2025-03-31 11:22:54 +0800

    refactor(生成器): 增加转换方法

--- generator.go
+++ generator.go
@@ -1,14 +1,10 @@
 package id
 
-import (
-	"github.com/goexl/id/internal"
-)
-
 // Generator 生成器
 type Generator interface {
 	// Next 下一个标识
-	Next() (internal.Value, error)
+	Next() (Value, error)
 
 	// Parse 从数字转换为标识
-	Parse(from uint64) internal.Value
+	Parse(from uint64) Value
 }
--- value.go
+++ value.go
@@ -1,4 +1,4 @@
-package internal
+package id
 
 import (
 	"time"
